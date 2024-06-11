29338326abe21f15416450fec2098383885b769b
Patrik Krizek <164909506+PatrikGetSetUK@users.noreply.github.com>
2024-06-11 22:15:44 +0100

    Week 8

---
+++ week-08/scripts/intro.js
@@ -0,0 +1,50 @@
+// JavaScript
+// SYNTAX
+
+// This is a comment.
+
+/* This is a
+multiple
+line
+comment */
+
+// LITERALS
+10;
+("Patrik");
+("Krizek");
+
+// VARIABLES
+var firstName = "Joe";
+
+const pi = 3.14;
+let myName = "Patrik";
+
+function myFunction() {
+  let myNumber = 8;
+}
+
+// ARITHMETIC OPERATORS
+(2 + 3) * 10;
+
+// NAMING VARIABLES
+const plant = "weed";
+
+// KEYWORDS
+// var let if function return
+
+let willChange = 0;
+willChange = 5;
+
+function myFunctionSum(a, b) {
+  return a - b;
+}
+
+console.log(myFunctionSum(10, 5));
+console.log(myFunctionSum(20, 8));
+console.log(myFunctionSum(20, 8));
+
+function addWords(c, d) {
+  return c + " " + d;
+}
+
+console.log(addWords("Patrik", "Krizek"));
